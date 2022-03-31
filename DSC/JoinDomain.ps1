0f94e7588b1b4e1b6453909d435df9c45ea968ad
Anthony <anthony@beckman-lapre.nl>
2022-03-31 15:01:30 +0200

    Add files via upload

---
+++ DSC/JoinDomain.ps1
@@ -0,0 +1,49 @@
+﻿<#PSScriptInfo
+.VERSION 1.0.0
+.GUID a8b9b735-a13d-4901-8edd-a2eb3a589183
+.AUTHOR DSC Community
+.COMPANYNAME DSC Community
+.COPYRIGHT Copyright the DSC Community contributors. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/dsccommunity/ComputerManagementDsc/blob/main/LICENSE
+.PROJECTURI https://github.com/dsccommunity/ComputerManagementDsc
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+#>
+
+#Requires -module ComputerManagementDsc
+
+<#
+    .DESCRIPTION
+        This configuration sets the machine name to 'Server01' and
+        joins the 'Contoso' domain.
+        Note: this requires an AD credential to join the domain.
+#>
+Configuration JoinDomain
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+        $DomainName,
+        $ComputerName
+
+    )
+
+    Import-DscResource -Module ComputerManagementDsc
+
+    Node localhost
+    {
+        Computer JoinDomain {
+            Name       = $ComputerName
+            DomainName = $DomainName
+            Credential = $Credential # Credential to join to domain
+        }
+    }
+}
